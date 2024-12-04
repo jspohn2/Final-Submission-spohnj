cbfd13ba3e16952d6e1d10117e97f4d198337e84
john.spohn <jspohn2@live.spcollege.edu>
2024-12-03 21:19:31 -0500

    dbcontext and connection string added

---
+++ BasicMVC/DataLayer/BasicDBContext.cs
@@ -0,0 +1,23 @@
+
+using BasicMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BasicMVC.DataLayer
+{
+    public class BasicDBContext : DbContext
+    {
+        public DbSet<Message> messages { get; set; } = null!;
+        public BasicDBContext(DbContextOptions<BasicDBContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Message>().HasData(
+                new Message { MessageID = 1, MessageText = "message one" },
+                new Message { MessageID = 2, MessageText = "message two" }
+            );
+        }
+    }
+}
